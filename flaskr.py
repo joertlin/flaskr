448506e1a511ce5bd5a755ebc14f218122d21e78
joertlin <jeremiah.e.oertling@gmail.com>
2015-11-08 21:10:39 -0600

    Debuged as much as I could.  This is the end of the tutorial.

--- flaskr.py
+++ flaskr.py
@@ -55,13 +55,13 @@ def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
-        elif request.form['username'] != app.config['PASSWORD']:
+        elif request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid password'
         else:
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
-        return render_template('login.html', error = error)
+    return render_template('login.html', error = error)
 
 @app.route('/logout')
 def logout():
